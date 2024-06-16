fa80f74530e61987db4a7d2595b2d08d457bc73f
Lakshmikanth Vallampati <kanth.vallampati@gmail.com>
2024-06-16 08:37:56 +0530

    chore: removed the wrong dist directory

--- dist/ivory-presentation/lib/services/data-manager.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class DataManagerService {
-    canSelectAll(dataSet: any): false | undefined;
-    static ɵfac: i0.ɵɵFactoryDeclaration<DataManagerService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<DataManagerService>;
-}
--- dist/ivory-presentation/lib/services/page-manager.service.d.ts
+++
@@ -1,9 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class PageManagerService {
-    private currentPageSub;
-    currentPage$: import("rxjs").Observable<number>;
-    updateCurrentPage(value: number): void;
-    resetPagination(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<PageManagerService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<PageManagerService>;
-}
